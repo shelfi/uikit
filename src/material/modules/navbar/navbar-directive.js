925ac8da3b6986cb23f954cea5712e6cd861c1ef
offminded <offminded@gmail.com>
2015-02-23 02:54:43 +0200

    modify home template + navbar tweaks

--- src/material/modules/navbar/navbar-directive.js
+++ src/material/modules/navbar/navbar-directive.js
@@ -4,27 +4,12 @@ module.exports = function (){
 	return {
 		restrict: 'E',
 		transclude: true,
-		template: getTemplate,
-		link: postLink,
+		templateUrl: 'modules/navbar/navbar.tmpl.html',
 		controller: 'NavbarCtrl',
 		controllerAs: '$NavbarCtrl',
 		scope: {
-			navsource: '=navsource'
+			navitems: '='
 		}
 	};
-
-	function postLink (scope, element, attr){
-
-	}
-
-	function getTemplate (){
-		return '<nav class="navbar__default">\
-				<md-button ng-click="$NavbarCtrl.isCollapsed = !$NavbarCtrl.isCollapsed" class="navbar-toggle" aria-label="Menu" show-sm hide>\
-				</md-button>\
-				<ul>\
-					<li ng-repeat="item in $NavbarCtrl.navitems"><a href="{{item.link}}">{{item.title}}</a></li>\
-				</ul>\
-				</nav>';
-				}
 		};
 })();
