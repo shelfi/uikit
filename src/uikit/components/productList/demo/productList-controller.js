9a04fd0e20dfc0adc5393aa4a8c0d54e012684d1
offminded <offminded@gmail.com>
2015-03-14 19:09:31 +0200

    modify productItem.scss for small screen layout product item + move price in product__info container

--- src/uikit/components/productList/demo/productList-controller.js
+++ src/uikit/components/productList/demo/productList-controller.js
@@ -7,7 +7,7 @@
 		.controller('productListCtrl', function () {
 
 			this.products = [
-				{ name: 'product 1', image: 'holder.js/100%x300', price: 15, desc: 'The titles of Washed Out\'s breakthrough song and the first single from Paracosm share the * two most important words in Ernest Greene\'s musical language: feel it. It\'s a simple request, as well...', promotion: [
+				{ name: 'Product with a really long name that can reach over two lines', image: 'holder.js/100%x300', price: 15, desc: 'The titles of Washed Out\'s breakthrough song and the first single from Paracosm share the * two most important words in Ernest Greene\'s musical language: feel it. It\'s a simple request, as well...', promotion: [
 					{ description: 'Free Shipping', icon: 'maps:ic_local_shipping_24px' }
 				]}, 
 				{ name: 'product 2', image: 'holder.js/100%x300', price: 30, desc: 'The titles of Washed Out\'s breakthrough song and the first single from Paracosm share the * two most important words in Ernest Greene\'s musical language: feel it. It\'s a simple request, as well...22222222222222222 33333333333333333' },
