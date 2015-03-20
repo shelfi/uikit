574be696e0d05e3fa52248adcd6c8992788ee4fb
uetkaje <emre.terzi@gurus.com.tr>
2015-03-20 13:16:24 +0200

    listService added to sf-product-list

--- src/uikit/components/productList/demo/productList-controller.js
+++ src/uikit/components/productList/demo/productList-controller.js
@@ -20,7 +20,6 @@
 				{ name: 'product 7', image: 'holder.js/100%x300', price: 45, desc: 'The titles of Washed Out\'s breakthrough song and the first single from Paracosm share the * two most important words in Ernest Greene\'s musical language: feel it. It\'s a simple request, as well...' },
 			];
 
-
 			this.actionButtons = {
 				compare: function (selectedItems) {
 					console.log('compare', selectedItems);
