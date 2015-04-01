fc72d76bd40cae9401c5a1fce1b9a9736e01806c
uetkaje <emre.terzi@gurus.com.tr>
2015-04-01 15:11:53 +0300

    Credit card form completed

--- src/uikit/components/creditCardForm/creditCardForm.js
+++ src/uikit/components/creditCardForm/creditCardForm.js
@@ -9,10 +9,14 @@
 
 		.directive('cardNumberValidator', function () {
 			return {
+				restrict: 'A',
 				require: 'ngModel',
-				link: function(scope, element, attrs, ngModel) {
-					ngModel.$validators.cardNumberValidator = function (value) {
-						return Payment.fns.validateCardNumber(value);
+				link: function(scope, element, attrs, ctrl) {
+					ctrl.$validators.cardNumberValidator = function (value) {
+						if (value) {
+							return Payment.fns.validateCardNumber(value);
+						}
+						return false;
 					};
 				}
 			};
@@ -20,22 +24,15 @@
 
 		.directive('cardExpiryValidator', function () {
 			return {
+				restrict: 'A',
 				require: 'ngModel',
-				link: function(scope, element, attrs, ngModel) {
-					ngModel.$validators.cardExpiryValidator = function (value) {
-						console.log(value);
-						
-						//TODO: accomplish this with regex!
-						
-						//console.log();
-						//var r = /^\d+\/\d+/$/.test(value);
-
-						//var r = value.split(' / ');
-						//console.log(r);
-
-
-						var month = '02', year = '2020';
-						return Payment.fns.validateCardExpiry(month, year);
+				link: function(scope, element, attrs, ctrl) {
+					ctrl.$validators.cardExpiryValidator = function (value) {
+						if (value) {
+							var expiry = Payment.fns.cardExpiryVal(value);
+							return Payment.fns.validateCardExpiry(expiry.month, expiry.year);
+						}
+						return false;
 					};
 				}
 			};
@@ -43,10 +40,14 @@
 
 		.directive('cardCvcValidator', function () {
 			return {
+				restrict: 'A',
 				require: 'ngModel',
-				link: function(scope, element, attrs, ngModel) {
-					ngModel.$validators.cardCvcValidator = function (value) {
-						return Payment.fns.validateCardCVC(value);
+				link: function(scope, element, attrs, ctrl) {
+					ctrl.$validators.cardCvcValidator = function (value) {
+						if (value) {
+							return Payment.fns.validateCardCVC(value);
+						}
+						return false;
 					};
 				}
 			};
@@ -55,9 +56,11 @@
 		.directive('sfCreditCardForm', function () {
 			return {
 				restrict: 'E',
+				require: '?ngModel',
 				templateUrl: 'components/creditCardForm/creditCardForm.tmpl.html',
 				scope: {
 					card: '=ngModel',
+					preview: '@',
 					proceed: '&'
 				},
 				bindToController: true,
@@ -67,17 +70,60 @@
 							alert('form validation failed');
 							return;
 						}
-						this.proceed();
+						this.proceed({ card: this.card });
 					};
 				},
 				controllerAs: 'ctrl',
-				link: function (scope, element) {
+				link: function (scope, element, attrs, ngModelCtrl) {
+					if (ngModelCtrl) {
+						ngModelCtrl.$formatters.push(function (v) {
+							
+							v.name = v.name || '';
+							v.number = v.number || '';
+							v.expiry = v.expiry || '';
+							v.cvc = v.cvc || '';
+
+							v.number = Payment.fns.formatCardNumber(v.number);
+
+							if (!Payment.fns.validateCardNumber(v.number)) {
+								v.number = '';
+							}
+
+							var expiry = Payment.fns.cardExpiryVal(v.expiry);
+							if (!Payment.fns.validateCardExpiry(expiry.month, expiry.year)) {
+								v.expiry = '';
+							}
+
+							if (!Payment.fns.validateCardCVC(v.cvc)) {
+								v.cvc = '';
+							}
+							return v;
+						});
+					}
+					//console.log('cardddd');
 					var card = new Card({
 						form: 'form[name="ctrl.creditCardForm"]',
 						container: '.card-wrapper',
 						width: 200,
 						formatting: true,
 						debug: true
+					});
+
+					scope.$watch('ctrl.card', function (val) {
+						//console.log('watchMAIN', val);
+
+						//http://stackoverflow.com/questions/446892/how-to-find-event-listeners-on-a-dom-node
+						//WebKit Inspector in Chrome or Safari browsers now does this. It will display the event listeners for a DOM element when you select it in the Elements pane.
+						//http://www.w3schools.com/jsref/met_document_addeventlistener.asp
+						//https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Creating_and_triggering_events
+						//http://stackoverflow.com/questions/18823872/jquery-dispatchevent-wrapper-method
+						//https://github.com/sandeep45/betterTrigger
+						//https://learn.jquery.com/events/triggering-event-handlers/
+
+						angular.element('input[name="name"]').simulate('keyup');
+						angular.element('input[name="number"]').simulate('keyup');
+						angular.element('input[name="expiry"]').simulate('keyup');
+						angular.element('input[name="cvc"]').simulate('keyup');
 					});
 				}
 			};
