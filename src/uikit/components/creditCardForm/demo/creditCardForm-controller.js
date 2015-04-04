a2b372ac2bf94398d0b1133ae037920e71474604
uetkaje <emre.terzi@gurus.com.tr>
2015-04-04 19:45:56 +0300

    Installment settings added to credit card form demo

--- src/uikit/components/creditCardForm/demo/creditCardForm-controller.js
+++ src/uikit/components/creditCardForm/demo/creditCardForm-controller.js
@@ -6,6 +6,50 @@
 
 		.controller('creditCardFormCtrl', function () {
 
+			this.updateInstallmentList = function () {
+				if (!this.amount_) {
+					this.amount_ = this.amount;
+				}
+				this.installments.forEach(function (v) {
+					if (v.rate > 0) {
+						var amount = (this.amount_ * 1) + (this.amount_ * v.rate / 100);
+						if (amount > this.amount) {
+							v.amount = amount - this.amount;
+						}
+						else {
+							v.amount = amount - this.amount;
+							//v.amount = ' -' + (this.amount - amount);
+						}
+					}
+					else {
+						delete v.amount;
+					}
+				}.bind(this));
+			};
+
+			this.amount = 100.56;
+			this.installments = [
+				{ installment: 1, rate: 0 },
+				{ installment: 3, rate: 0 },
+				{ installment: 6, rate: 1.3 },
+				{ installment: 9, rate: 2.6 }
+			];
+			this.updateInstallmentList();
+
+			this.installmentChanged = function () {
+				var selected = this.installments.filter(function (v) {
+					return v.installment === parseInt(this.installment);
+				}.bind(this));
+				
+				this.amount = this.amount_;
+
+				if (selected[0].rate > 0) {
+					this.amount = (this.amount_ * 1) + (this.amount_ * selected[0].rate / 100);
+				}
+
+				this.updateInstallmentList();
+			};
+
 			this.common = {
 				name: 'Ahmet Niyazi',
 				number: '4929233596865177',
