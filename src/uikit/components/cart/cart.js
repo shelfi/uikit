5384db1db208c6651d5c71b012d25a65694e6e61
uetkaje <emre.terzi@gurus.com.tr>
2015-04-10 14:50:35 +0300

    Closed #2 - Cart completed

--- src/uikit/components/cart/cart.js
+++ src/uikit/components/cart/cart.js
@@ -21,73 +21,84 @@
 			];
 		})
 
-		.directive('sfCart', function ($mdTheming, $mdDialog) {
+		.directive('sfCart', function () {
 			return {
 				restrict: 'E',
 				templateUrl: 'components/cart/cart.tmpl.html',
 				scope: {
-					items: '=ngModel',
-					columns: '=',
+					order: '=ngModel',
+					columns: '=?',
+					totals: '=?',
+					actions: '=?',
 					showHeader: '@',
-					showDeleteButton: '@',
-					showStandbyButton: '@',
-					showTotals: '@',
-					deleteAction: '&',
-					standbyAction: '&'
+					showGroupAction: '@',
+					removeItem: '&'
 				},
 				bindToController: true,
 				controller: function () {
-
-					var buttons = {
-						name: 'buttons',
-						width: '5',
-						template: ''
+					this.triggerActionCallback = function (cb) {
+						var items = this.order.products.filter(function (p) {
+							return p.selected === true;
+						});
+						if (items.length) {
+							cb(items);
+						}
 					};
-
-					if (this.showDeleteButton === 'true') {
-
-						this.deleteItem = function (item) {
-
-							var confirm = $mdDialog.confirm()
-								.title('Would you like to delete?')
-								//.content('All of the banks have agreed to forgive you your debts.')
-								//.ariaLabel('Lucky day')
-								.ok('Yes')
-								.cancel('No');
-								//.targetEvent(ev);
-
-							$mdDialog.show(confirm).then(function () {
-								var index = this.items.indexOf(item);
-								if (index !== -1) {
-									this.items.splice(index, 1);
-								}
-							}.bind(this));
-						};
-						
-						buttons.template += '<md-button aria-label="remove from cart" ng-click="ctrl.deleteItem(item)"><md-icon md-svg-icon="navigation:ic_close_24px"></md-icon></md-button>';
-					}
-
-					if (this.showStandbyButton === 'true') {
-
-						this.standbyItem = function (item) {
-
-							console.log('standby', item);
-						};
-
-						buttons.template += '<md-button aria-label="remove from cart" ng-click="ctrl.standbyItem(item)"><md-icon md-svg-icon="navigation:ic_unfold_more_24px"></md-icon></md-button>';
-					}
-
-					if (buttons.template.length > 0) {
-						this.columns.push(buttons);	
-					}
 				},
-				controllerAs: 'ctrl'
-				//link: postLink,
-			};
+				controllerAs: 'ctrl',
+				link: function (scope, element, attrs) {
+					if (!attrs.columns) {
+						scope.ctrl.columns = [
+							{ name: 'image', title: 'Image', width: '10', template: '<img holder-js="{{ item.image }}" />' },
+							{ name: 'name', title: 'Name' },
+							{ name: 'qty', title: 'Qty', width: '15', template: '<md-input-container ng-if="!item.qtyOptions"><label>Quantity</label><input ng-model="item.qty"></md-input-container><md-select ng-if="item.qtyOptions" ng-model="item.qty" placeholder="Select quantity"><md-option ng-repeat="qtyOption in item.qtyOptions" ng-value="qtyOption">{{ qtyOption }}</md-option></md-select>' },
+							{ name: 'total', title: 'Total', width: '10', template: '{{ item.price * item.qty | currency }}' }
+						];
+					}
 
-			//function postLink (scope, element){
-			//	$mdTheming(element);
-			//}
+					if (scope.ctrl.showGroupAction === 'true') {
+						scope.ctrl.columns.unshift({
+							name: 'select',
+							title: 'Select',
+							width: '5',
+							template: '<md-checkbox ng-model="item.selected" aria-label="Select item"></md-checkbox>'
+						});
+					}
+
+					if (!attrs.totals) {
+						scope.ctrl.totals = [
+							{
+								name: 'total',
+								title: 'Total',
+								fn: function (order) {
+									return order.products.reduce(function (returned, current) {
+										return returned + (current.price * current.qty);
+									}, 0);
+								}
+							}
+						];
+					}
+					scope.ctrl.actions = scope.ctrl.actions || [];
+					if (attrs.removeItem) {
+						scope.ctrl.actions.unshift({
+							name: 'remove',
+							title: 'Remove',
+							icon: 'navigation:ic_close_24px',
+							cb: function (items) {
+								scope.ctrl.removeItem({ items: items });
+							}
+						});
+					}
+					if (scope.ctrl.actions.length) {
+						scope.ctrl.columns.push({
+							name: 'actions',
+							title: 'Actions',
+							width: '5',
+							template: '<md-button ng-repeat="action in ctrl.actions" aria-label="{{ action.title }}" ng-click="action.cb(item)"><md-icon md-svg-icon="{{ action.icon }}"></md-icon></md-button>'
+						});
+					}
+				}
+			};
 		});
 
 })();
--- src/uikit/templates/shop/views/cart/cart-controller.js
+++ src/uikit/templates/shop/views/cart/cart-controller.js
@@ -23,13 +23,77 @@
 			
 
 			this.columns = [
-				{ name: 'image', width: '10', template: '<img holder-js="{{ item.image }}" />' },
-				{ name: 'name', width: '', template: '<small>{{item.name}}</small>' },
-				{ name: 'price', width: '10' },
-				//{ name: 'quantity', width: '10', template: '<md-input-container><label>Quantity</label><input ng-model="item.quantity"></md-input-container>' }
-				//{ name: 'quantity', width: '10', template: '<md-input-container><md-select ng-model="item.quantity" placeholder="Select quantity"><md-option ng-repeat="opt in [100, 250, 500, 1000, 2500]" ng-value="opt">{{ opt }}</md-option></md-select></md-input-container>'
-				{ name: 'quantity', width: '15', template: '<md-select ng-model="item.quantity" placeholder="Select quantity"><md-option ng-repeat="opt in [100, 200, 500, 1000, 5000]" ng-value="opt">{{ opt }}</md-option></md-select>' }
+				{ name: 'total', title: 'Total', width: '10', template: '{{ item.price * item.qty | currency }}' },
+				{ name: 'image', title: 'Image', width: '10', template: '<img holder-js="{{ item.image }}" />' },
+				{ name: 'name', title: 'Name' },
+				{ name: 'qty', title: 'Qty', width: '15', template: '<md-input-container ng-if="!item.qtyOptions"><label>Quantity</label><input ng-model="item.qty"></md-input-container><md-select ng-if="item.qtyOptions" ng-model="item.qty" placeholder="Select quantity"><md-option ng-repeat="qtyOption in item.qtyOptions" ng-value="qtyOption">{{ qtyOption }}</md-option></md-select>' }
 			];
+
+
+
+			this.actions = [
+				{
+					name: 'standby',
+					title: 'Standby',
+					icon: 'navigation:ic_more_vert_24px',
+					cb: function (items) {
+						console.log('Standby', items);
+					}
+				}
+			];
+
+
+			this.removeItem = function (items) {
+				console.log('REMOVE', items);
+
+
+				/*
+				var confirm = $mdDialog.confirm()
+					.title('Would you like to delete?')
+					//.content('All of the banks have agreed to forgive you your debts.')
+					//.ariaLabel('Lucky day')
+					.ok('Yes')
+					.cancel('No');
+					//.targetEvent(ev);
+
+				$mdDialog.show(confirm).then(function () {
+					var index = this.items.indexOf(item);
+					if (index !== -1) {
+						this.items.splice(index, 1);
+					}
+				}.bind(this));
+				*/
+			};
+
+			/*
+			if (this.showStandbyButton === 'true') {
+
+				this.standbyItem = function (item) {
+
+					console.log('standby', item);
+				};
+
+				buttons.template += '<md-button aria-label="remove from cart" ng-click="ctrl.standbyItem(item)"><md-icon md-svg-icon="navigation:ic_unfold_more_24px"></md-icon></md-button>';
+			}
+			*/
+
+
+
+
+
+			/*
+			this.columns = [
+				{ name: 'image', width: '10', template: '<img holder-js="{{ item.image }}" />' },
+				{ name: 'name', width: '' },
+				{ name: 'price', width: '10', template: '<small>{{ item.price | currency }}</small>' },
+				{ name: 'qty', width: '15', template: '<md-input-container ng-if="!item.qtyOptions"><label>Quantity</label><input ng-model="item.qty"></md-input-container><md-select ng-if="item.qtyOptions" ng-model="item.qty" placeholder="Select quantity"><md-option ng-repeat="opt in item.qtyOptions" ng-value="opt">{{ opt }}</md-option></md-select>' }
+			];
+
+			this.totals = [
+				{ title: 'vat', fn: function () { return 4; } },
+				{ title: 'total', fn: function () { return 16; } }
+			];
+			*/
 		});
 
 })();
